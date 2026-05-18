c31d3a77e43bc41fe44ac7faa1870f90e8ad7043
GANGHWI LEE <rkdgnlee@gmail.com>
2026-05-18 16:32:42 +0900

    composition 크기 고정

--- src/components/left/Composition.tsx
+++ src/components/left/Composition.tsx
@@ -252,7 +252,7 @@ export default function Composition({data}: {data: IBiaData}) {
         </div>
       </div>
 
-      <div className="grid grid-cols-[20%_80%] mt-2 px-4 py-2 bg-sub-100 border border-sub-200 rounded-md items-center ">
+      <div className="flex gap-2 h-10 mt-2 px-4 bg-sub-100 border border-sub-200 rounded-md items-center ">
           <span className="font-bold text-sub-800 text-xs text-center">{title}</span>
           <span className="text-sub-800 text-[10px] leading-none">{description}</span>
       </div>
